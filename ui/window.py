6bd060e8e8a5673bf1e48db6259946c60ca5a3a2
YukiYuzuriha <joskiytawer@gmail.com>
2025-11-20 15:04:17 +0300

    add debug text on button click in ui/window.py

--- ui/window.py
+++ ui/window.py
@@ -13,11 +13,7 @@ class MainWindow(QMainWindow):
         
         self.prev_button = QPushButton("previous", self)
         self.next_button = QPushButton("next", self)
-        self.switch_button = QPushButton("placeholder!", self)
-
-        # TODO: ui shows play or pause button depending on timer state
-        # or better logic here
-
+        self.switch_button = QPushButton("play!", self)
         self.folder_button = QPushButton("switch folder...", self)
 
         layout.addWidget(self.prev_button)
@@ -25,8 +21,24 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.switch_button)
         layout.addWidget(self.folder_button)
         
+        self.prev_button.clicked.connect(self._on_prev)
+        self.next_button.clicked.connect(self._on_next)
+        self.switch_button.clicked.connect(self._on_switch)
+        self.folder_button.clicked.connect(self._on_folder)
+
         center.setLayout(layout)
         self.setCentralWidget(center)
 
-        
-        
+    def _on_prev(self):
+        print("prev??")
+
+    def _on_next(self):
+        print("next??")
+
+    def _on_switch(self):
+        print("switch??")
+
+    def _on_folder(self):
+        print("folder??")
+
+    
