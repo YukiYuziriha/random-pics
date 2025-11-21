f960c32d77d697c4288f6e5ae5ff4336458317ba
YukiYuzuriha <joskiytawer@gmail.com>
2025-11-21 21:10:36 +0300

    add aplication/controller.py class AppController to handle layering domain logic with ui

---
+++ application/controller.py
@@ -0,0 +1,20 @@
+from domain.playlist import Playlist 
+
+class AppController():
+    def __init__(self) -> None:
+        self.playlist = Playlist([])
+    
+    def on_folder_selected(self, paths: list[str]) -> str | None:
+        self.playlist = Playlist(paths)
+
+        return self.current()
+
+    def current(self) -> str | None:
+        return self.playlist.current()
+
+    def on_next(self) -> str | None:
+        return self.playlist.next()
+
+    def on_prev(self) -> str | None:
+        return self.playlist.prev()
+
