69ca02b2c6655b8d3f8d4e76a7aaf58d099dddbb
YukiYuzuriha <joskiytawer@gmail.com>
2026-01-14 12:23:03 +0300

    src/imgLoader.ts

--- src/imgLoader.ts
+++ src/imgLoader.ts
@@ -1,11 +1,19 @@
-import { join } from "deno.land";
-import { userHomeDir } from "@std/os";
+import { readdir } from "node:fs/promises";
+import { join } from "node:path";
 
-const dirPath = join(await userHomeDir(), "Downloads");
+const dirPath = "/mnt/ssd/refs/800 Rebel Girl Fighter/Spear";
 
-for await (const entry of Deno.readDir(dirPath)) {
-  if (entry.isFile && /\.(jpg|jpeg|png|webp|gif)$/i.test(entry.name)) {
-    console.log(`Found image: ${entry.name}`);
+const files = await readdir(dirPath);
+const images = [];
 
+for (const file of files) {
+  if (/\.(jpg|jpeg|png|webp|gif)$/i.test(file)) {
+    
+    console.log(`image ${file}`); //just list images
+    
+    const pathToImage = `${dirPath}/${file}`;
+    const buffer = await Bun.file(pathToImage).arrayBuffer();
+    images.push(Buffer.from(buffer));
   }
 }
+
